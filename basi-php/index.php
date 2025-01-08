7f56f7809d802deb3567fdbe2703405c0b52d15f
captain-mike <info@grafikweb.it>
2025-01-08 20:01:39 +0100

    basi-php

---
+++ basi-php/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+   <h1><?php echo 'Hello World!' ?></h1>
+
+   <?php echo '<b>Generato da PHP</b>' ?>
+
+   <?php
+   
+   if(false){
+    echo 'vero';
+   }else{
+    echo 'falso';
+   }
+
+   ?>
+
+</body>
+</html>
