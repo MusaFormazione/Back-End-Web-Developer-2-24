e1274cab1e6fdabf55f628de3899087e92a4fcf0
captain-mike <info@grafikweb.it>
2025-04-23 19:33:14 +0200

    pagination oop

---
+++ L28/paginazione-oop/classes/Connection.php
@@ -0,0 +1,21 @@
+<?php
+
+class Connection
+{
+    private $db;
+
+    public function __construct(string $dbName, string $username = '', string $password = '', string $host = "localhost")
+    {
+        try {
+            $this->db = new PDO("mysql:dbname=$dbName;host=$host", $username, $password);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->db;
+    }
+
+}
